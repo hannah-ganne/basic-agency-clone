9bbf207025b0ff66549e344741d2a32c445bd501
lanomade <ganne.hannah@gmail.com>
2022-10-14 21:23:15 +0200

    create styled components for hero section

---
+++ src/styles/Hero.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { Section } from './GlobalStyle'
+
+export const HeroContainer = styled(Section)`
+    padding-top: 5.5rem;
+    padding-bottom: 5.5rem;
+`
+
+export const HeroText = styled.h1`
+    font-size: 2.3rem;
+`
+
+
+
