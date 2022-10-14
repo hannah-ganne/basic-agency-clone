518f63a5e7d4f7bb9fa64b06edae0977a7d727b7
lanomade <ganne.hannah@gmail.com>
2022-10-14 23:13:03 +0200

    add button and logo in the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
-import { HeroContainer, HeroText } from "../styles/Hero";
-import { Row, Column } from '../styles/GlobalStyle'
+import { HeroContainer, HeroText, HeroLogo } from "../styles/Hero";
+import { Row, Column, Button } from '../styles/GlobalStyle'
+import logoSimple from '../assets/images/logo-simple.svg'
 
 export default function Hero() {
     return (
@@ -9,6 +10,10 @@ export default function Hero() {
                     <HeroText>
                         BASIC/DEPT® is a global branding and digital design agency building products, services, and eCommerce experiences that turn cultural values into company value.
                     </HeroText>
+                    <Button>See the work</Button>
+                </Column>
+                <Column>
+                    <HeroLogo src={logoSimple} />
                 </Column>
             </Row>
         </HeroContainer>
