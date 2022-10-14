5a77519e2f2faf28d4c4f8bf529c1ec49ca48906
lanomade <ganne.hannah@gmail.com>
2022-10-14 23:13:59 +0200

    change the drag selection color

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,4 +1,5 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, keyframes } from "styled-components";
+import noiseBackground from '../assets/images/300-30-10-monochrome.png'
 
 const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap');
@@ -21,10 +22,16 @@ const GlobalStyle = createGlobalStyle`
         font-family: 'Lato', sans-serif;
         position: relative;
         overflow: scroll;
+        background-color: var(--white);
     }
 
     *, *::before, *::after {
         box-sizing: inherit;
+    }
+
+    ::selection {
+        color: var(--white);
+        background: var(--black);
     }
 
     a {
@@ -42,16 +49,72 @@ const GlobalStyle = createGlobalStyle`
 export default GlobalStyle
 
 export const Section = styled.section`
+    width: 100vw;
     padding-left: 5rem;
     padding-right: 5rem;
+    overflow: hidden;
 `
 
 export const Row = styled.div`
+    width: 100%;
     display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
 `
 
 export const Column = styled.div`
     display: flex;
     flex-direction: column;
     flex: 0 0 calc((var(--grid-width) + var(--grid-gutter) - var(--grid-padding)*2)/var(--grid-column-count)*6 - var(--grid-gutter));
+`
+
+export const Button = styled.button`
+    width: fit-content;
+    height: 2rem;
+    border: 1px solid var(--black);
+    border-radius: 10rem;
+    text-transform: uppercase;
+    font-size: 0.7rem;
+    font-weight: 700;
+    padding: 0 1.5rem;
+    cursor: pointer;
+    background: linear-gradient(to top, var(--black) 50%, var(--white) 50%);
+    background-size: 100% 200%;
+    background-position: top;
+    transition: all .3s ease-out;
+    margin-top: 2.5rem;
+
+    &:hover {
+        background-position: bottom;
+        border-color: var(--white);
+        color: var(--white)
+    }
+`
+const noiseAnimation = keyframes`
+    0% { transform: translate(0,0) }
+    10% { transform: translate(-5%,-5%) }
+    20% { transform: translate(-10%,5%) }
+    30% { transform: translate(5%,-10%) }
+    40% { transform: translate(-5%,15%) }
+    50% { transform: translate(-10%,5%) }
+    60% { transform: translate(15%,0) }
+    70% { transform: translate(0,10%) }
+    80% { transform: translate(-15%,0) }
+    90% { transform: translate(10%,5%) }
+    100% { transform: translate(5%,0) }
+`
+
+export const Noise = styled.div`
+    position: absolute;
+    top: -50%;
+    left: -50%;
+    right: -50%;
+    bottom: -50%;
+    width: 200%;
+    height: 200vh;
+    background-image: url(${noiseBackground});
+    background-repeat: repeat;
+    animation: ${noiseAnimation} .2s infinite;
+    opacity: 0.9;
+    visibility: visible;
 `
