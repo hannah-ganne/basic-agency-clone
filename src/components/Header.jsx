ca055b569ca60f4dd19b205007bc4f274b25e46b
lanomade <ganne.hannah@gmail.com>
2022-10-15 00:12:14 +0200

    remove unused code and pass props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, Logo, Nav, NavItem, NavLink } from '../styles/Header'
+import logo from '../assets/images/logo.svg'
 import logoWhite from '../assets/images/logo-white.svg'
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { useEffect, useState } from 'react'
@@ -6,61 +7,48 @@ import { useEffect, useState } from 'react'
 export default function Header() {
     const [currentScroll, setCurrentScroll] = useState(0)
     const [lastScroll, setLastScroll] = useState(0)
+    const scrollUp = currentScroll < lastScroll
+    const scrollDown = currentScroll > lastScroll
+    const scrollDone = currentScroll <= 700
 
-
-    useEffect(() => {
-        const body = document.body
-        const scrollUp = "scroll-up"
-        const scrollDown = "scroll-down"
-        
+    useEffect(() => {       
         window.addEventListener("scroll", () => {
             setCurrentScroll(window.pageYOffset)
-
-            // if (currentScroll <= 83) {
-            //     body.classList.remove(scrollUp);
-
-            //     return;
-            // }
-
-            // if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
-            //   // down
-            //     body.classList.remove(scrollUp);
-            //     body.classList.add(scrollDown);
-
-            // } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
-            //   // up
-            //     body.classList.remove(scrollDown);
-            //     body.classList.add(scrollUp);
-            // }
-
             setLastScroll(currentScroll)
-        });
+        })
+
+        return (() => {
+            window.addEventListener("scroll", () => {
+                setCurrentScroll(window.pageYOffset)
+                setLastScroll(currentScroll)
+            })
+        })
     })
 
     return (
-        <HeaderContainer scrollDown={currentScroll > lastScroll ? true: false} scrollDone={currentScroll <= 700 ? true : false}>
-            <Logo src={logoWhite} alt="logo of basic agency" />
-            <Nav>
-                <NavItem>
-                    <NavLink href="#">Work</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="#">About</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="#">News</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="#">Thinking</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="#">Careers</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="#">Contact</NavLink>
-                </NavItem>
-            </Nav>
-            <MoreHorizIcon sx={{ color: '#f4f4f4'}} />
-        </HeaderContainer>
+        <HeaderContainer scrollUp={scrollUp} scrollDown={scrollDown} scrollDone={scrollDone}>
+                <Logo src={scrollDone? logoWhite : logo} alt="logo of basic agency" />
+                <Nav>
+                    <NavItem>
+                        <NavLink href="#">Work</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">About</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">News</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">Thinking</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">Careers</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="#">Contact</NavLink>
+                    </NavItem>
+                </Nav>
+                <MoreHorizIcon sx={scrollDone ? { color: '#f4f4f4'} : { color: '#252422'}} />
+            </HeaderContainer>
     )
 }
