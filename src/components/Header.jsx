bb1e317ad35e54175da58fa69cf5c6aa9bc11f3c
lanomade <ganne.hannah@gmail.com>
2022-10-14 23:12:45 +0200

    add sticky header functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,44 @@
 import { HeaderContainer, Logo, Nav, NavItem, NavLink } from '../styles/Header'
 import logoWhite from '../assets/images/logo-white.svg'
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { useEffect, useState } from 'react'
 
 export default function Header() {
+    const [currentScroll, setCurrentScroll] = useState(0)
+    const [lastScroll, setLastScroll] = useState(0)
+
+
+    useEffect(() => {
+        const body = document.body
+        const scrollUp = "scroll-up"
+        const scrollDown = "scroll-down"
+        
+        window.addEventListener("scroll", () => {
+            setCurrentScroll(window.pageYOffset)
+
+            // if (currentScroll <= 83) {
+            //     body.classList.remove(scrollUp);
+
+            //     return;
+            // }
+
+            // if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+            //   // down
+            //     body.classList.remove(scrollUp);
+            //     body.classList.add(scrollDown);
+
+            // } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+            //   // up
+            //     body.classList.remove(scrollDown);
+            //     body.classList.add(scrollUp);
+            // }
+
+            setLastScroll(currentScroll)
+        });
+    })
+
     return (
-        <HeaderContainer>
+        <HeaderContainer scrollDown={currentScroll > lastScroll ? true: false} scrollDone={currentScroll <= 700 ? true : false}>
             <Logo src={logoWhite} alt="logo of basic agency" />
             <Nav>
                 <NavItem>
