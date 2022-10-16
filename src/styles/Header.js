1240578e5672a88d9e0e596cec71b7f16af6f783
lanomade <ganne.hannah@gmail.com>
2022-10-17 00:44:06 +0200

    adjust z-index

--- src/styles/Header.js
+++ src/styles/Header.js
@@ -9,7 +9,7 @@ export const HeaderContainer = styled.header`
     position: fixed;
     top: 0;
     left: 0;
-    z-index: 5;
+    z-index: 1;
     ${({scrollDown}) => scrollDown && `
         transform: translateY(-100%);
         transition: all 0.3s ease-in;
@@ -20,7 +20,7 @@ export const HeaderContainer = styled.header`
         top: 0;
         left: 0;
         right: 0;
-        z-index: 100;
+        z-index: 5;
         transition: all 0.3s ease-in;
         & a {
             color: var(--black);
