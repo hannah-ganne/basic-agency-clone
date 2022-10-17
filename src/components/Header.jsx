2bc77f0d38e2ac9a9d94ce5ab39e59a48ddf263a
lanomade <ganne.hannah@gmail.com>
2022-10-17 21:50:24 +0200

    change the color for dark theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { HeaderContainer, Logo, Nav, NavItem, NavLink } from '../styles/Header'
 import logo from '../assets/images/logo.svg'
 import logoWhite from '../assets/images/logo-white.svg'
+import logoPink from '../assets/images/logo-pink.svg'
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { useEffect, useState } from 'react'
 
-export default function Header() {
+export default function Header({ isVisible }) {
     const [currentScroll, setCurrentScroll] = useState(0)
     const [lastScroll, setLastScroll] = useState(0)
     const scrollUp = currentScroll < lastScroll
@@ -27,7 +28,7 @@ export default function Header() {
 
     return (
         <HeaderContainer scrollUp={scrollUp} scrollDown={scrollDown} scrollDone={scrollDone}>
-                <Logo src={scrollDone? logoWhite : logo} alt="logo of basic agency" />
+                <Logo src={scrollDone? logoWhite : isVisible ? logoPink : logo} alt="logo of basic agency" />
                 <Nav>
                     <NavItem>
                         <NavLink href="#">Work</NavLink>
@@ -48,7 +49,11 @@ export default function Header() {
                         <NavLink href="#">Contact</NavLink>
                     </NavItem>
                 </Nav>
-                <MoreHorizIcon sx={scrollDone ? { color: '#f4f4f4'} : { color: '#252422'}} />
+            <MoreHorizIcon
+                sx={scrollDone ? { color: '#f4f4f4' } :
+                    isVisible ? { color: '#f9cdcd' } :
+                        { color: '#252422' }} 
+            />
             </HeaderContainer>
     )
 }
